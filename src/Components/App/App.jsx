ba840ea5e01e2dbf5b29a074caea736c2e25c2f7
Brock Benson <brockjbenson@outlook.com>
2023-03-10 12:06:07 -0600

    landing page done. Almost done with about page style

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,10 +1,20 @@
+import LandingMessage from "../LandingMessage/LandingMessage";
 import Nav from "../Nav/Nav";
 import "./App.css";
+import About from "../About/About";
 
 function App() {
+  const handleClickScroll = () => {
+    const element = document.getElementById("about-section");
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
   return (
     <div className="App">
       <Nav />
+      <LandingMessage handleClickScroll={handleClickScroll} />
+      <About id="about-section" />
     </div>
   );
 }
